0c9f973b1ba790d8164e9106be789ae929bcc459
nber106 <nber106@aucklanduni.ac.nz>
2021-08-12 21:20:46 +1200

    starting to look like a simulation

--- board.py
+++ board.py
@@ -1,7 +1,8 @@
 import random
 class Board:
 
-    def __init__(self, size: int):
+    def __init__(self, size: int, max_queens: int):
+        self.solution = True
         row = 0
         this_board = []
         self.queens = 0
@@ -11,7 +12,7 @@ class Board:
             column = 0
             while column<size:
                 #new_row.append(f"[{column},{row}]")
-                if (random.randint(0,7)==0):
+                if (random.randint(0,7)==0 and self.queens<max_queens):
                     new_row.append("[Q]")
                     self.queens += 1
                 else:
@@ -37,13 +38,29 @@ class Board:
             while x < size:
                 square = self.board[y][x]
                 if self.board[y][x][1]=="Q":
-                    self.queenCoordinates.append([x,y])
+                    self.queenCoordinates.append((x,y))
                 x+=1
             y+=1
         print("Queens are located at: "+str(self.queenCoordinates))
 
     def checkQueenCaptures(self):
+        for queen in self.queenCoordinates:
+            this_x = queen[0]
+            this_y = queen[1]
+            for other_queen in self.queenCoordinates:
+                if (this_x==other_queen[0] and this_y==other_queen[1]):
+                    None
+                    #this is the same queen so ignore
+                elif (this_x==other_queen[0]):
+                    print(f"The Queen at ({this_x},{this_y}) can capture the Queen at ({other_queen[0]},{other_queen[1]}) vertically")
+                    self.solution = False
+                elif (this_y == other_queen[1]):
+                    print(f"The Queen at ({this_x},{this_y}) can capture the Queen at ({other_queen[0]},{other_queen[1]}) horizontally")
+                    self.solution = False
 
-
-
-
+    def isASolution(self):
+        if self.solution:
+            print(f"This board solves the {self.queens} Queens Problem")
+        else:
+            print(f"Queens can capture, this is not a solution.")
+        return self.solution
--- main.py
+++ main.py
@@ -9,9 +9,20 @@ queen_square = "[Q]"
 
 #row_initial
 
-
-if __name__ == '__main__':
-    test_board = Board(8)
+def generateABoard():
+    test_board = Board(8,8)
     print(test_board)
     test_board.render()
     test_board.getQueenCoordinates()
+    test_board.checkQueenCaptures()
+    return test_board.isASolution()
+
+if __name__ == '__main__':
+    num_tests = 1000
+    test_index = 0
+    solution_count = 0
+    while test_index < num_tests:
+        if generateABoard():
+            solution_count+=1
+        test_index+=1
+    print(f"The puzzle was randomly solved in {solution_count} of {num_tests} tests.")
